4722271220491fddb531f6d98849ef873b1a37e4
k3vina <kevinajepkonga@gmail.com>
2026-08-30 19:41:02 +0300

    Add the view task, mark task as complete, delete task, and exit to the menu, add few tasks to the menu

--- main.py
+++ main.py
@@ -2,25 +2,62 @@ from database import TaskDatabase
 from task import Task
 import sqlite3
 from datetime import date
+import os
 
+# connects the CLI interface to the database
 connection = sqlite3.connect("tasks.db")
 db = TaskDatabase(connection)
 #print("Done!")
 
 while True:
-    print("Add Task")
-    print("View Task")
-    print("Mark Complete")
+    #if os.name == "nt":
+    #    os.system("cls")
+    #else:
+    #    os.system("clear")
+
+
+    print("Add task")
+    print("View task")
+    print("Mark complete")
     print("Delete")
     print("Exit")
     menu = input(f"Enter an option: ")
+    #print(repr(menu))
 
-    if menu == "Add Task":
+    # adds tasks to the database
+    if menu == "Add task":
         id = None
         title = input("Enter the title of your task: ")
         description = input("Enter the description: ")
-        due_date = input("Enetr today's date: ")
+        due_date = input("Enter the due date: ")
         completed = 0
         task = Task(id, title, description, due_date, completed)
         db.add_task(task)
-    
+
+    # lets the user view the tasks written
+    if menu == "View task":
+        task_list = db.get_all_tasks()
+        print(f"DEBUG: found {len(task_list)} tasks")
+        for position, task in enumerate(task_list, start=1):
+            print(f"Task {position}")
+            task.display_info()
+        
+
+    # marks tasks complete once done
+    if menu == "Mark complete":
+        task_id = int(input("Which the id of the task you want to mark as complete? "))
+        db.mark_complete(task_id)
+
+    # deletes a task
+    if menu == "Delete":
+        task_id = int(input("Which task do you want to delete? "))
+        db.delete_task(task_id)
+
+    # exits the menu system
+    if menu == "Exit":
+        db.connection.commit()
+        db.connection.close()
+        print("Goodbye! See you later.")
+        break
+
+
